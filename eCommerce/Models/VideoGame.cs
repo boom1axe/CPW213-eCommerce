a2acb4c52f543ca500e5d7cc3454f320b92207c8
Dennis Kafton <boom1axe@att.net>
2019-07-23 09:49:55 -0700

    Created starting video game class

---
+++ eCommerce/Models/VideoGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents a single video game
+    /// </summary>
+    public class VideoGame
+    {
+        /// <summary>
+        /// Unique ID number 
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The game title
+        /// </summary>
+        [Required]
+        [StringLength(90)]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Official ESRB rating
+        /// </summary>
+        public string Rating { get; set; }
+
+        /// <summary>
+        /// Game description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Game price
+        /// </summary>
+        [Range(0.01, 999.99)]
+        [DataType(DataType.Currency)] // Required by default because double is a value type
+        public double Price { get; set; }
+    }
+}
