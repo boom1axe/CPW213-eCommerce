751e6cb834b4205b548af3825ce15f9fdb1eef43
Dennis Kafton <boom1axe@att.net>
2019-08-08 10:21:53 -0700

    added missing primary key to Member

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -11,6 +11,9 @@ namespace eCommerce.Models
     /// </summary>
     public class Member
     {
+        [Key]
+        public int MemberId { get; set; }
+
         /// <summary>
         /// The first and last name of the member.
         /// ex.
