a57da36f206167fb44abc189b1d4852fbf75ebb9
Dennis Kafton <boom1axe@att.net>
2019-08-27 09:18:10 -0700

    created seach criteria results

---
+++ eCommerce/Models/SearchCriteria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class SearchCriteria
+    {
+        public string Title { get; set; }
+        public string Rating { get; set; }
+
+        [Display(Name = "Minimum Price")]
+        [Range(0, double.MaxValue, ErrorMessage = "the min price must be double")]
+        public double? MinPrice { get; set; }
+
+        [Display(Name = "Maximum Price")]
+        [Range(0, double.MaxValue, ErrorMessage = "the Max price must be double")]
+        public double? MaxPrice { get; set; }
+
+        /// <summary>
+        /// The video games found using the search criteria
+        /// </summary>
+        public List<VideoGame> Games { get; set; }
+
+
+    }
+}
